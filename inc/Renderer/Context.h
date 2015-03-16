bd873459a0ea41b3f77607d69a602a0b60f7dfa6
sanya <okay123456@yandex.ru>
2015-03-16 17:19:46 +0300

    wrap reference to Drawable in unique_ptr

--- inc/Renderer/Context.h
+++ inc/Renderer/Context.h
@@ -4,25 +4,25 @@
 #include "Renderer/Buffer.h"
 #include "Renderer/Device.h"
 #include "Renderer/Drawable.h"
-
+#include <memory>
 namespace Renderer{
 
 class Context{
-protected:
+private:
 	Buffer * buffers;
 	int32_t current_buffer;
 	size_t buffers_num;
 	std::vector<Connector> connectors;
 	Crtc saved_crtc;
 	Device &dev;
-	Drawable & drawable;
+	std::unique_ptr<Drawable>  drawable;
 	void changeBuffer();
 public:
 	class ContextException : public std::runtime_error{
 	public:
 		using std::runtime_error::runtime_error;
 	};
-	Context(size_t buffers_num, Connector &conn, Crtc & crtc, Drawable &drawable	, Device & dev);
+	Context(size_t buffers_num, Connector &conn, Crtc & crtc, 	std::unique_ptr<Drawable> &&drawable	, Device & dev);
 	~Context();
 	void addConnector(Connector & );
 	void startListenning();
