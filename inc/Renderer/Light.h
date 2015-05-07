44ee6d5903bbdafccc7ac8b1a4315d7caacf9105
sanya <okay123456@yandex.ru>
2015-05-07 18:54:01 +0300

    light skelet added

---
+++ inc/Renderer/Light.h
@@ -0,0 +1,20 @@
+#ifndef LIGHT_H_
+#define LIGHT_H_
+
+class DiffuseLight{
+//TODO mf
+};
+
+class AmbientLight{
+//TODO mf
+};
+
+class SpecularLight{
+//TODO mf
+};
+
+class Phong{
+//TODO mf
+};
+
+#endif /* LIGHT_H_ */
